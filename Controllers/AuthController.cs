c8d5fbe3db76492bc88ecebff3bc745ba51d0a5b
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2025-12-31 13:49:03 +0300

    Create auth controller to handle login and refresh token endpoints.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Azure;
+using LifeHub_APIs.Dtos;
+using LifeHub_APIs.Models;
+using LifeHub_APIs.services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifeHub_APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IAuthService service) : ControllerBase
+    {
+        [HttpPost("Login")]
+        public async Task<ActionResult<LoginResponse>> Login(LoginDto request)
+        {
+            var response = await service.LoginAsync(request);
+            if (response is null)
+                return BadRequest("user name or password incorrect");
+            return Ok(response);
+        }
+        public async Task<ActionResult<LoginResponse>> RefreshTokens(int userId, string refreshToken)
+        {
+            var response = service.RefreshTokensAsync(userId, refreshToken);
+            if (response is null)
+                return BadRequest();
+            return Ok(response);
+        }
+    }
+}
