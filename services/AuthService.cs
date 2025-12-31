1a32663ec192b4ed56f4ab06a2af1235e2666e7e
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2025-12-31 16:07:56 +0300

    1. Remove token service and use this metods in the service: issue with injecting classes fixed with this step. 2. Create register user service.

--- services/AuthService.cs
+++ services/AuthService.cs
@@ -3,11 +3,32 @@ using LifeHub_APIs.Dtos;
 using LifeHub_APIs.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace LifeHub_APIs.services
 {
-    public class AuthService(AppDbContext context, TokenService tokens): IAuthService
+    public class AuthService(AppDbContext context, IConfiguration configuration): IAuthService
     {
+        public async Task<User?> RegisterAsync(LoginDto request)
+        {
+            var existingUser = await context.Users.FirstOrDefaultAsync(u => u.Username == request.UserName);
+            if (existingUser != null)
+                return null;
+            var user = new User();
+            var hashedPassword = new PasswordHasher<User>().HashPassword(user, request.Password);
+
+            user.Username = request.UserName;
+            user.PasswordHash = hashedPassword;
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            return user;
+
+        }
         public async Task<LoginResponse?> LoginAsync(LoginDto request)
         {
             var user = await context.Users.FirstOrDefaultAsync(u => u.Username == request.UserName);
@@ -18,14 +39,79 @@ namespace LifeHub_APIs.services
             if (requestedPassword == PasswordVerificationResult.Failed)
                 return null;
 
-            return await tokens.GetTokenResponse(user);
+            return await GetTokenResponse(user);
         }
         public async Task<LoginResponse?> RefreshTokensAsync(int userId, string refreshToken)
         {
-            var user = await tokens.validateRefreshToken(userId, refreshToken);
+            var user = await validateRefreshToken(userId, refreshToken);
             if (user is null)
                 return null;
-            return await tokens.GetTokenResponse(user);
+            return await GetTokenResponse(user);
+        }
+        public async Task<User?> GetUserAsync(int id)
+        {
+            var user = await context.Users.FindAsync(id);
+            return user;
+        }
+
+        /***** Tokens handlers *****/
+        private async Task<LoginResponse> GetTokenResponse(User user)
+        {
+            return new LoginResponse
+            {
+                AccessToken = GenerateAccessToken(user),
+                RefreshToken = await GenerateAndSaveRefreshToken(user),
+                UserName = user.Username,
+                ExpirationDate = new JwtSecurityTokenHandler().ReadJwtToken(GenerateAccessToken(user)).ValidTo
+            };
+        }
+        private async Task<User?> validateRefreshToken(int userId, string refreshToken)
+        {
+            var user = await context.Users.FindAsync(userId);
+            if (user is null || user.RefreshToken != refreshToken
+               || user.RefreshTokenExpiryTime <= DateTime.UtcNow)
+                return null;
+            return user;
+        }
+
+        private string GenerateAccessToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTSettings:SecretKey"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+            DateTime tokenTime = DateTime.UtcNow.AddMinutes(15);
+            var tokenDescriptor = new JwtSecurityToken
+            (
+                issuer: configuration["JWTSettings:Issuer"]!,
+                audience: configuration["JWTSettings:Audience"]!,
+                claims: claims,
+                expires: tokenTime,
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+        }
+
+        private async Task<string> GenerateAndSaveRefreshToken(User user)
+        {
+            var refreshToken = GenerateRefreshToken();
+            user.RefreshToken = refreshToken;
+            user.RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(7);
+            await context.SaveChangesAsync();
+            return refreshToken;
+        }
+
+
+        private string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[32];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
         }
     }
 }
--- services/IAuthService.cs
+++ services/IAuthService.cs
@@ -5,7 +5,9 @@ namespace LifeHub_APIs.services
 {
     public interface IAuthService
     {
+        Task<User?> RegisterAsync(LoginDto user);
         Task<LoginResponse?> LoginAsync(LoginDto user);
         Task<LoginResponse?> RefreshTokensAsync(int userId, string refreshToken);
+        Task<User?> GetUserAsync(int id);
     }
 }
