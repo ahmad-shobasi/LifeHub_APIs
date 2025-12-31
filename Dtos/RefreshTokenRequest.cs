bed917056cfa108eca614d063590dcee3e7a4318
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2025-12-31 16:09:49 +0300

    Create dto to handle refreshing token request.

---
+++ Dtos/RefreshTokenRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace LifeHub_APIs.Dtos
+{
+    public class RefreshTokenRequest
+    {
+        public int UserId { get; set; }
+        public required string RefreshToken { get; set; }
+    }
+}
