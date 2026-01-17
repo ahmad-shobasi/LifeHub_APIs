f2682d47df2086804cbecac2c0a60867696db0e9
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2026-01-17 10:40:56 +0300

    Create dtos and models.

--- Dtos/TaskRequestDto.cs
+++ Dtos/TaskRequestDto.cs
@@ -1,13 +1,11 @@
-﻿namespace LifeHub_APIs.Models.DailyTasks
+﻿using LifeHub_APIs.Models.DailyTasks;
+
+namespace LifeHub_APIs.Dtos
 {
-    public class DailyTask
+    public class TaskRequestDto
     {
-        public int id { get; set; }
-        public int UserId { get; set; }
-        public User User { get; set; }
         public required string Title { get; set; }
         public string Description { get; set; } = String.Empty;
         public PriorityFlag priority { get; set; } = PriorityFlag.Done;
-
     }
 }
