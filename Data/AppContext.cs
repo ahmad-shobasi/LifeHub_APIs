99919ddd22ae11a709145691dd1911a2e32bacd1
Ahmad-shobasi <132388281+ahmad-shobasi@users.noreply.github.com>
2026-01-17 10:40:10 +0300

    Update the context and add user tasks relation.

--- Data/AppContext.cs
+++ Data/AppContext.cs
@@ -1,4 +1,5 @@
 ﻿using LifeHub_APIs.Models;
+using LifeHub_APIs.Models.DailyTasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace LifeHub_APIs.Data
@@ -6,5 +7,17 @@ namespace LifeHub_APIs.Data
     public class AppDbContext(DbContextOptions<AppDbContext> options): DbContext(options)
     {
         public DbSet<User> Users => Set<User>();
+        public DbSet<DailyTask> Tasks => Set<DailyTask>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DailyTask>()
+                .HasOne(t => t.User)
+                .WithMany(u => u.tasks)
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+
     }
 }
